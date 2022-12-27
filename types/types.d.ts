8f4615745239c6d27f98d827c1ad9ab5ce723b99
AdneCode <super-gijs@hotmail.com>
2022-12-27 18:55:07 +0100

    feat: types

---
+++ types/types.d.ts
@@ -0,0 +1,90 @@
+////Client & server shared types////
+//Gamestates
+export type GamePhase = 'PreGame' | 'Preparing' | 'InGame' | string;
+
+//Players
+type PlayerId = number | null;
+export type Player = {
+    name: string;
+    id: string;
+    playerId: PlayerId;
+};
+type Players = Player[];
+
+//Game and room related
+export type Game = {
+    id: string;
+    hostId: string;
+    players: Players;
+    phase: GamePhase;
+    size: number;
+    round: number;
+    maxRounds: number;
+    timelines: number;
+    timelineTime: number;
+    timer: number | null;
+};
+export type Room = Game;
+
+interface ServerToClientEvents {
+    error: (error: string) => void;
+    receiveIx: (ix: number) => void;
+    game: (game: Game) => void;
+}
+
+interface ClientToServerEvents {
+    register: (gameId: string) => void;
+    start: (gameId: string) => void;
+    roll: (gameId: string) => void;
+    makeBid: (gameId: string) => void;
+}
+
+//socketData
+export type Data = {
+    newRoom?: Room;
+    playerName?: string;
+    roomId?: string;
+    room?: Game;
+    settings?: Object;
+};
+
+////End Client & server shared types////
+
+////Client related types////
+export type State = {
+    connected: boolean;
+    error: string | null;
+    playerIx: number | null;
+    game: Game | null;
+};
+export type RawState = {
+    gameState: {
+        connected: boolean;
+        error: string | null;
+        playerIx: number | null;
+        game: Game | null;
+    };
+};
+
+//Actions
+export type Action =
+    | { type: 'SET_CONNECTED' }
+    | { type: 'SET_ERROR'; error: string }
+    | { type: 'IX_RECEIVED'; ix: number }
+    | { type: 'GAME_RECEIVED'; game: Game }
+    | { type: 'PHASE_CHANGE'; phase: GamePhase }
+    | { type: 'ARENA_RECEIVED'; arena: Game | any };
+
+//Props
+export type GameProp = {
+    game: Game;
+    className?: string;
+};
+////End client related types////
+
+//Server related types////
+export type Rooms = Game[] | [];
+interface SocketData {
+    playerIx: number;
+}
+////End Server related types////

41a70ebc93ae6642c73f116ca84b44e0fe0c47cd
AdneCode <super-gijs@hotmail.com>
2023-02-23 02:53:13 +0100

    feat: offset to messages and scroll

--- client/src/components/sections/Messages.tsx
+++ client/src/components/sections/Messages.tsx
@@ -1,5 +1,5 @@
 import { GameProp } from '../../../../globalUtility/types';
-import { useEffect, useContext } from 'react';
+import { useEffect, useContext, useState } from 'react';
 import { messageReducer, messageArrayReducer } from '../../store';
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
 import { SocketContext } from '../../socket/socket';
@@ -7,14 +7,18 @@ import { Data } from '../../../../globalUtility/types';
 import { selectMessages } from '../../store';
 
 export const Messages = (p: GameProp) => {
+    const messageOffset = 3;
     const socket = useContext(SocketContext);
     const dispatch = useAppDispatch();
     const messages = useAppSelector(selectMessages());
+    const [offset, setOffset] = useState<number>(messageOffset);
+
     const { game } = p;
     useEffect(() => {
         socket.on('receiveMessage', (data: Data) => {
             if (!data.message) return;
             dispatch(messageReducer(data.message));
+            setOffset(messageOffset);
         });
         return () => {
             socket.off('receiveMessage');
@@ -26,18 +30,55 @@ export const Messages = (p: GameProp) => {
         dispatch(messageArrayReducer(game.messages));
     }, [game]);
 
+    useEffect(() => {
+        const onScroll = (e: any) => {
+            if (
+                e.deltaY > 0 &&
+                messages &&
+                offset < messages.length - messageOffset
+            ) {
+                setOffset(offset + 1);
+            }
+            if (e.deltaY < 0 && messages && offset > messageOffset) {
+                setOffset(offset - 1);
+            }
+        };
+        console.log(offset);
+        window.removeEventListener('wheel', onScroll);
+        window.addEventListener('wheel', onScroll, { passive: true });
+        return () => {
+            window.removeEventListener('wheel', onScroll);
+        };
+    }, [offset]);
+
     return (
         <div>
-            <DisplayedMessages messages={messages} />
+            <DisplayedMessages
+                messages={messages}
+                messageOffset={messageOffset}
+                offset={offset}
+            />
         </div>
     );
 };
 
-const DisplayedMessages = (p: { messages: string[] | null }) => {
-    const { messages } = p;
+const DisplayedMessages = (p: {
+    messages: string[] | null;
+    messageOffset: number;
+    offset: number;
+}) => {
+    const { messages, messageOffset, offset } = p;
     if (!messages) return <>No messages</>;
-    const renderedMessages = messages.map((i: string) => {
-        return <h1>{i}</h1>;
+    const renderedMessages = messages.map((i: string, index: number) => {
+        console.log('OFFSET', offset);
+        console.log(index);
+
+        if (index - messageOffset < offset && index + messageOffset > offset)
+            return (
+                <div key={index}>
+                    <h1>{i}</h1>
+                </div>
+            );
     });
     return <>{renderedMessages}</>;
 };

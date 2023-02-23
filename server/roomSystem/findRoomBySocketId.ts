87bab4cf89b7aaf1d9cc0df730c35e282e4c898d
AdneCode <super-gijs@hotmail.com>
2023-02-23 13:09:54 +0100

    fix: find in function

--- server/roomSystem/findRoomBySocketId.ts
+++ server/roomSystem/findRoomBySocketId.ts
@@ -1,8 +1,8 @@
 import { Room } from '../../globalUtility/types';
 import { roomHasSocketId } from './roomHasSocketId';
 //return the room the socketId is in that is provided
-export const findRoomBySocketId = (rooms, socketId) => {
-    return rooms.map((i: Room) => {
+export const findRoomBySocketId = (rooms: Room[], socketId: string) => {
+    return rooms.find((i: Room) => {
         return roomHasSocketId(i, socketId);
     });
 };

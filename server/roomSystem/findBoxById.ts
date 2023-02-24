4c598f1b0a3c3c094bc1f69b49d80255ab57874c
AdneCode <super-gijs@hotmail.com>
2023-02-24 20:13:02 +0100

    feat: explici

--- server/roomSystem/findBoxById.ts
+++ server/roomSystem/findBoxById.ts
@@ -1,6 +1,6 @@
 import { Box, Room } from '../../globalUtility/types';
 
-export const findBoxById = (room: Room, boxId: number) => {
+export const findBoxById = (room: Room, boxId: number): Box => {
     const foundBox = room.boxes.find((i: Box) => {
         return i.id === boxId;
     });

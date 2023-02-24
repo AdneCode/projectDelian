f84aab4c300f4d56c65dbedfc311825e0cab5f42
AdneCode <super-gijs@hotmail.com>
2023-02-24 20:24:37 +0100

    fix: return

--- server/roomSystem/generateNewRooms.ts
+++ server/roomSystem/generateNewRooms.ts
@@ -1,5 +1,5 @@
 import { Room, Rooms } from '../../globalUtility/types';
-export const generateNewRooms = (rooms: Rooms, newRoom: Room) => {
+export const generateNewRooms = (rooms: Rooms, newRoom: Room): Rooms => {
     return rooms.map((i: Room) => {
         if (i.id === newRoom.id) return newRoom;
         return i;
